b95eb3352137917ba162089bd71474706a102fe1
Charles Lu <clu2033@gmail.com>
2017-08-07 18:52:11 -0400

    bug fixes

--- code/bot.py
+++ code/bot.py
@@ -34,48 +34,50 @@ def main():
 		socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, '127.0.0.1', 9050)
 		socket.socket = socks.socksocket	
 		for i in range(100):
-			print('\nbot:', i, '\taddress:', get_ip_address(controller))
-			recs = parse_html(get_html(url_to_scrape))
+			print('\n######################################################')
+			print('\tIteration:',i+1,'\taddress:',get_ip_address(controller))
+			recs_before = parse_html(get_html(url_to_scrape))
 			STEP = 0
-			csv_writer(FILE, recs, controller)
-			get_html(choose_random_rec(recs)) #throwaway
-			recs = parse_html(get_html(url_to_scrape))
+			csv_writer(FILE, recs_before, controller)
+			get_html(choose_random_rec(recs_before)) #throwaway
+			recs_after = parse_html(get_html(url_to_scrape))
 			STEP = 1
-			csv_writer(FILE, recs,controller)
-			controller.signal(Signal.NEWNYM) # new ip address
-			time.sleep(10)
-			controller.signal(Signal.NEWNYM) # new ip address
-			time.sleep(10)
+			csv_writer(FILE, recs_after, controller)
 			controller.signal(Signal.NEWNYM) # new ip address
 			time.sleep(10)
 
 ###########################################################################
 
 def get_html(url):
-	response = requests.get(url).text
-	return response
+	response = requests.get(url)
+	if not response.status_code == 200: 
+		print(response.status_code)
+	return response.text
 
 def parse_html(html):
 	vid_stat = []
 	soup = BeautifulSoup(html,'lxml')		
-	rec = soup.find('ul', {'id':'watch-related'})
-	for i, v in enumerate(rec.find_all(attrs={'class':'content-wrapper'})):
-		stat = []
-		for span in v.find_all('span'):
-			s = span.text
-			s = s.lstrip().rstrip()
-			stat.append(s)
-		stat.append(YOUTUBE+v.a.get('href'))
-		vid_stat.append(stat)
+	try:
+		rec = soup.find('ul', {'id':'watch-related'})
+		for i, v in enumerate(rec.find_all(attrs={'class':'content-wrapper'})):
+			stat = []
+			for span in v.find_all('span'):
+				s = span.text
+				s = s.lstrip().rstrip()
+				stat.append(s)
+			stat.append(YOUTUBE+v.a.get('href'))
+			vid_stat.append(stat)
+	except:
+		print(soup.prettify())
 	return vid_stat
 
 def get_bot_id(file, exists=True):
 	if exists:
 		with open(file, 'r') as f:
 			last_line = f.readlines()[-1].split(',')
-			if int(last_line[3]) == 0:
+			if int(last_line[3]) == 1:
 				bid = int(last_line[0])+1
-			elif int(last_line[3]) == 1:
+			elif int(last_line[3]) == 0:
 				bid = int(last_line[0])
 	else:
 		bid = 1
@@ -91,8 +93,9 @@ def get_ip_address(controller):
 	return address
 
 def choose_random_rec(vids):
-	url = vids[random.randint(1,19)][-1]
-	print('\tRandomly chose', url)
+	# print('\n', len(vids), '\n', vids, '\n')
+	url = vids[random.randint(0,19-1)][-1]
+	print('\tRandomly chose', url, '\n')
 	return url
 
 def csv_writer(file, video_stats, controller):
